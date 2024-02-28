b936ca6c70a5c887edc65f210668fc222949d81c
arvind singharpuria <arvind.020606@gmail.com>
2024-02-28 18:09:46 +0530

    created random bucket name function

--- resources/__main__.py
+++ resources/__main__.py
@@ -1,8 +1,15 @@
 import pulumi
 import pulumi_aws as aws
+import random
+import string
+
+def generate_random_bucket_name(length=10):
+    return ''.join(random.choices(string.ascii_lowercase, k=length))
+
+bucket_name = generate_random_bucket_name()
 
 # Create an AWS S3 bucket
-s3_bucket = aws.s3.Bucket("my_bucket")
+s3_bucket = aws.s3.Bucket(bucket_name)
 
 # Export the name of the bucket
 pulumi.export('bucket_name', s3_bucket.id)
