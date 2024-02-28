2c7b343aa309f977cc6279009241b864185bd01d
arvind singharpuria <arvind.020606@gmail.com>
2024-02-28 19:50:58 +0530

    pulumi port environment

--- resources/__main__.py
+++ resources/__main__.py
@@ -1,15 +1,59 @@
+"""An AWS Python Pulumi program"""
+
 import pulumi
-import pulumi_aws as aws
-import random
-import string
+import s3bucket
+import sqs
+import port
+import lambda_function
+from pulumi.output import Inputs
 
-def generate_random_bucket_name(length=10):
-    return ''.join(random.choices(string.ascii_lowercase, k=length))
+config = pulumi.Config()
+ENVIRONMENT = pulumi.get_stack()
+BUCKET_ACL = config.get("bucketAcl") or "private"
 
-bucket_name = generate_random_bucket_name()
+BUCKET_BLUEPRINT_IDENTIFIER = "s3Bucket"
+QUEUE_BLUEPRINT_IDENTIFIER = "sqsQueue"
+LAMBDA_BLUEPRINT_IDENTIFIER = "lambda"
+DEVENV_BLUEPRINT_IDENTIFIER = "devEnv"
 
-# Create an AWS S3 bucket
-s3_bucket = aws.s3.Bucket(bucket_name)
+# Create a new bucket
+bucket = s3bucket.S3Bucket(
+    f"port-pulumi-webinar-{ENVIRONMENT}",
+    {"acl": BUCKET_ACL},
+)
 
-# Export the name of the bucket
-pulumi.export('bucket_name', s3_bucket.id)
+pulumi.export("Bucket Name", bucket.bucket.id)
+
+# Create a new SQS queue
+queue = sqs.SqsQueue(f"port-pulumi-webinar-{ENVIRONMENT}")
+pulumi.export("Queue Name", queue.queue.name)
+
+# Create a new Lambda function
+lambda_function = lambda_function.LambdaFunction(f"port-pulumi-webinar-{ENVIRONMENT}")
+pulumi.export("Lambda Name", lambda_function.lambda_function.name)
+
+# Create a new dev-env entity
+dev_env_entity = port.PortEntity(
+    "devEnv",
+    f"port-pulumi-webinar-{ENVIRONMENT}-dev-env",
+    f"Port Pulumi Webinar {ENVIRONMENT} Developer Environment",
+    properties=[
+        {
+            "name": "env",
+            "value": f"{ENVIRONMENT}",
+        },
+    ],
+    relations=[
+        {
+            "name": "lambda",
+            "identifier": lambda_function.lambda_function_entity.identifier,
+        },
+        {"name": "s3Bucket", "identifier": bucket.bucket_entity.identifier},
+        {"name": "sqsQueue", "identifier": queue.sqs_queue_entity.identifier},
+    ],
+    dependencies=[
+        bucket.bucket,
+        queue.queue,
+        lambda_function.lambda_function,
+    ],
+)
